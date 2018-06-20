03c722bcd50298a5459025869b3379d21a8254f0
Patrick Ineichen <github@frokonet.ch>
2018-06-20 15:47:30 +0200

    Fixed some issues

--- ng-football-pools/src/app/bets/bets.component.ts
+++ ng-football-pools/src/app/bets/bets.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { AuthService } from '../auth/auth.service';
@@ -12,17 +13,25 @@ import { MatchWithBet } from './matchWithBet.model';
   templateUrl: './bets.component.html',
   styleUrls: ['./bets.component.scss']
 })
-export class BetsComponent implements OnInit {
+export class BetsComponent implements OnInit, OnDestroy {
   matches: MatchWithBet[];
+
+  private subscription: Subscription;
 
   constructor(private authService: AuthService, private betsService: BetsService) {}
 
   ngOnInit() {
     const userId = this.authService.getLoginState().id;
-    this.betsService
+    this.subscription = this.betsService
       .getAllByUserId(userId)
       .pipe(map(match => match.filter(m => m.date > this.currentDate())))
       .subscribe(m => (this.matches = m));
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   save() {
